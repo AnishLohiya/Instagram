82175408f33899e4bc2e9fc367078ecd8c009d97
Anish <87910771+AnishLohiya@users.noreply.github.com>
2023-05-03 12:53:42 +0530

    more changes

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -1,9 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:instagram/screens/profile_screen.dart';
 import 'package:instagram/utils/colors.dart';
-import 'package:instagram/utils/global_variables.dart';
 
 class SearchScreen extends StatefulWidget {
   const SearchScreen({Key? key}) : super(key: key);
